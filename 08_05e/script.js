73e8e9f7ab245410f845fe30881df388d73c2712
lawrenceaph <76676277+lawrenceaph@users.noreply.github.com>
2021-07-20 20:02:58 +0800

    edited article

--- 08_05e/script.js
+++ 08_05e/script.js
@@ -26,3 +26,55 @@ const greenPack = {
 };
 
 console.log(greenPack.newVolume(5));
+
+
+const mech1 = {
+
+  name: "ubermech",
+  color: "gray",
+  guns: 50,
+  lasers: 10,
+  powerlevel: 100,
+  newpowerlevel: function (newpowerlevel) {
+     this.powerlevel = newpowerlevel;
+     return(console.log("the new mech power level is:", this.powerlevel));
+  } 
+};
+
+console.log("Mech1 Power Level:", mech1.powerlevel);
+const mechdatasheet = document.getElementById("mechdata");
+const imagis = document.getElementById("imageo");
+
+function showMechProperties () {
+mechdatasheet.innerHTML=
+  `
+  <p class="container has-text-centered">Mech Properties:</p>
+
+  <p class="has-text-centered">Super color : ${mech1.color}
+
+  </p>
+  `;
+  mechdatasheet.style.display = 'block'
+  imagis.style.display = 'block'
+};
+
+function hideMechProperties () {
+mechdatasheet.style.display = 'none';
+imagis.style.display = 'none';
+}
+
+const button1 = document.getElementById("uberbutton");
+const button2 = document.getElementById("uberbutton2");
+button1.addEventListener('click', () => {
+  showMechProperties()
+  button1.style.display = 'none'
+  button2.style.display = 'flex'
+  
+
+});
+
+button2.addEventListener('click', () => {
+  hideMechProperties()
+  button2.style.display = 'none'
+  button1.style.display = 'flex'
+})
